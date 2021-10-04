f38d2f3cafd09d9869baacd14da0725b7a610c2c
Francis <15980933+yinhaofrancis@users.noreply.github.com>
2021-10-05 01:02:25 +0800

    t

---
+++ AlphaTests/ObjectTest.swift
@@ -0,0 +1,71 @@
+//
+//  HeapTest.swift
+//  AlphaTests
+//
+//  Created by hao yin on 2021/8/17.
+//
+
+import XCTest
+@testable import Alpha
+class ObjectTest: XCTestCase {
+
+    let poor:DataBasePool = try! DataBasePool(name: "m")
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    func testModel() throws{
+        let a = a()
+        a.string = "dadad"
+        a.stringw = "asdadasd"
+        a.a = Int(arc4random())
+        self.poor.writeSync { db in
+            try a.create(db: db)
+            try a.insert(db: db)
+            let pp = try ObjectRequest<a>(table: "a").query(db: db)
+            
+            print(pp)
+            
+        }
+    }
+}
+class a:Object{
+    
+    @Col
+    var string:String = ""
+    
+    @NullableCol
+    var stringw:String?
+    
+    @Col([.primary])
+    var a:Int = 0
+}
+class b:Object{
+    override var name: String{
+        return "a"
+    }
+    @Col
+    var strsdfsdfsi:String = ""
+    
+    @NullableCol
+    var stringw:String?
+    
+    @Col([.primary])
+    var a:Int = 0
+}
+class c:Object{
+    @Col
+    var cc:String = ""
+    
+    @NullableCol
+    var bb:String?
+    
+    @Col
+    var aa:Int = 0
+    
+    @NullableCol
+    var fa:a?
+}
