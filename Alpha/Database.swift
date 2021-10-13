17d9faf56deebeb4a60be58eef6e94af81e60a5c
yinhao <yinhao@5eplay.com>
2021-10-13 10:41:20 +0800

    doc

--- Alpha/Database.swift
+++ Alpha/Database.swift
@@ -8,6 +8,7 @@
 import Foundation
 import SQLite3
 import SQLite3.Ext
+/// Wal 模式
 public struct WalMode:RawRepresentable{
     public init?(rawValue: Int32) {
         self.rawValue = rawValue
@@ -24,6 +25,7 @@ public struct WalMode:RawRepresentable{
     public static var truncate  = WalMode(rawValue: SQLITE_CHECKPOINT_TRUNCATE)!
     
 }
+/// 外键约束模式
 public struct ForeignKeyAction:Hashable{
     let action:String
     static public var CASCADE:ForeignKeyAction = ForeignKeyAction(action: "CASCADE")
@@ -33,6 +35,7 @@ public struct ForeignKeyAction:Hashable{
     static public var SET_DEFAULT:ForeignKeyAction = ForeignKeyAction(action: "SET DEFAULT")
 }
 
+/// 数据 写入 回滚 模式
 public enum JournalMode:String{
     case DELETE
     case TRUNCATE
@@ -41,6 +44,7 @@ public enum JournalMode:String{
     case WAL
     case OFF
 }
+/// 数据同步模式
 public enum synchronousMode:String{
     case EXTRA
     case FULL
@@ -52,12 +56,15 @@ public class Database:Hashable{
     public static func == (lhs: Database, rhs: Database) -> Bool {
         lhs.hashValue == rhs.hashValue
     }
-    private var uuid = UUID().uuidString
     public func hash(into hasher: inout Hasher) {
-        hasher.combine(uuid)
+        hasher.combine(url)
     }
     public let url:URL
     public var sqlite:OpaquePointer?
+    ///  创建Sqlite3 数据库
+    /// - Parameters:
+    ///   - url: 数据库地址
+    ///   - readOnly: 只读
     public init(url:URL,readOnly:Bool = false) throws{
         self.url = url
         let r = readOnly ? SQLITE_OPEN_READONLY | SQLITE_OPEN_NOMUTEX  : (SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX)
@@ -67,8 +74,9 @@ public class Database:Hashable{
         }
         self.hook()
     }
+    /// sqlite 内存数据库
     public init() throws{
-        self.url = URL(string: "file:memdb1?mode=memory&cache=shared")!
+        self.url = URL(string: "file:\(arc4random())DB?mode=memory&cache=shared")!
         sqlite3_open(url.path, &self.sqlite)
         if(self.sqlite == nil){
             throw NSError(domain: "create memory sqlite3 fail", code: 0, userInfo: [:])
@@ -96,15 +104,20 @@ public class Database:Hashable{
         }
         
         private var stmt:OpaquePointer
-        private var nextSql:String?
         unowned var db:Database
-        public init(stmt:OpaquePointer,db:Database,nextSql:String? = nil) {
+        /// sqlite3 结果集合
+        /// - Parameters:
+        ///   - stmt:stmt
+        ///   - db: 数据库
+
+        public init(stmt:OpaquePointer,db:Database) {
             self.stmt = stmt
             self.db = db
-            self.nextSql = nextSql
         }
-        @discardableResult
-        public func step() throws ->Bool{
+        
+        /// 步进到下一条
+        /// - Returns: 是否有下一条
+        @discardableResult public func step() throws ->Bool{
             let rc = sqlite3_step(self.stmt)
             if rc == SQLITE_ROW{
                 return true
@@ -115,9 +128,15 @@ public class Database:Hashable{
             
             throw NSError(domain: Database.errormsg(pointer: self.db.sqlite!), code: Int(rc), userInfo: ["sql":String(cString: sqlite3_sql(self.stmt))])
         }
+        /// 获取列名
+        /// - Parameter index: 列号
+        /// - Returns: 列名
         public func columnName(index:Int)->String{
             String(cString: sqlite3_column_name(self.stmt, Int32(index)))
         }
+        /// 数据库列类型
+        /// - Parameter index: 列号
+        /// - Returns: 列类型
         public func type(index:Int32)->DBDataType{
             let a = sqlite3_column_type(self.stmt, index)
             switch a {
@@ -133,19 +152,28 @@ public class Database:Hashable{
                 return .Null
             }
         }
+        /// 关闭
         public func close(){
             sqlite3_finalize(self.stmt)
         }
+        /// 列数
         public var columnCount:Int{
             Int(sqlite3_column_count(self.stmt))
         }
+        /// 查询列号
+        /// - Parameter paramName: 列名
+        /// - Returns: 列号
         public func index(paramName:String)->Int32{
             let index = sqlite3_bind_parameter_index(self.stmt, paramName)
             return index
         }
+        /// 列号绑定参数
+        /// - Returns: 绑定数据对象
         public func bind<T>(index:Int32)->Bind<T>{
             Bind<T>.init(stmt: self.stmt, index: index)
         }
+        /// 列名绑定参数
+        /// - Returns: 绑定数据对象
         public func bind<T>(name:String)->Bind<T>?{
             let index = self.index(paramName: name)
             if(index == 0){
@@ -153,25 +181,40 @@ public class Database:Hashable{
             }
             return Bind<T>.init(stmt: self.stmt, index: index)
         }
+        /// 绑定空
+        /// - Parameter index: 参数id 用‘?’为占位符
         public func bindNull(index:Int32){
             sqlite3_bind_null(self.stmt, index)
         }
+        /// 绑定空
+        /// - Parameter name: 参数名 eg：@{参数名}
         public func bindNull(name:String){
             let index = self.index(paramName: name)
             sqlite3_bind_null(self.stmt, index)
         }
+        
+        /// 获取列
+        /// - Parameter paramName: 列号
+        /// - Parameter type: 类型
+        /// - Returns: 列
         public func column<T>(index:Int32,type:T.Type)->Column<T>{
             Column.init(stmt: self.stmt, index: index)
-        }        
+        }
+        /// 参数总数
         public var paramCount:Int32{
             sqlite3_bind_parameter_count(self.stmt)
         }
+        /// 绑定参数对象
         public struct Bind<T>{
             public var stmt:OpaquePointer
             public var index:Int32
+            /// 绑定数据
+            /// - Parameter value: 整数
             public func bind(value:T) where T == Int32{
                 sqlite3_bind_int(self.stmt, index, value)
             }
+            /// 绑定数据
+            /// - Parameter value: 整数
             public func bind(value:T) where T == Int{
                 if MemoryLayout.size(ofValue: value) == 4{
                     sqlite3_bind_int(self.stmt, index, Int32(value))
@@ -179,15 +222,21 @@ public class Database:Hashable{
                     sqlite3_bind_int64(self.stmt, index, Int64(value))
                 }
             }
+            /// 绑定数据
+            /// - Parameter value: 整数
             public func bind(value:T) where T == Int64{
                 sqlite3_bind_int64(self.stmt, index, value)
             }
+            /// 绑定数据
+            /// - Parameter value: 浮点数
             public func bind(value:T) where T == Double{
                 sqlite3_bind_double(self.stmt, index, value)
             }
             public func bind(value:T) where T == Float{
                 sqlite3_bind_double(self.stmt, index, Double(value))
             }
+            /// 绑定数据
+            /// - Parameter value: 字符串
             public func bind(value:T) where T == String{
                 guard let c = value.cString(using: .utf8) else {
                     sqlite3_bind_null(self.stmt, index)
@@ -199,9 +248,12 @@ public class Database:Hashable{
                     p?.deallocate()
                 }
             }
+            /// 重制
             public func reset(){
                 sqlite3_reset(self.stmt)
             }
+            /// 绑定数据
+            /// - Parameter value: data
             public func bind(value:T) where T == Data{
                 let pointer = sqlite3_malloc(Int32(value.count))
                 let buffer = UnsafeMutableRawBufferPointer(start: pointer, count: value.count)
@@ -210,30 +262,44 @@ public class Database:Hashable{
                     sqlite3_free(po)
                 }
             }
+            /// 参数名
             public var name:String{
                 String(cString: sqlite3_bind_parameter_name(self.stmt, self.index))
             }
         }
+        /// 列对象
         public struct Column<T>{
             public var stmt:OpaquePointer
             public var index:Int32
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == Int32{
                 sqlite3_column_int(self.stmt, index)
             }
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == Int64{
                 sqlite3_column_int64(self.stmt, index)
             }
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == String{
                 let len = sqlite3_column_bytes(self.stmt, index)
                 guard let byte = sqlite3_column_text(self.stmt, index) else { return "" }
                 return String(data: Data(bytes: byte, count: Int(len)), encoding: .utf8) ?? ""
             }
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == Double{
                 sqlite3_column_double(self.stmt,index)
             }
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == Float{
                 Float(sqlite3_column_double(self.stmt,index))
             }
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == Int{
                 if MemoryLayout<T>.size == 4{
                     return Int(sqlite3_column_int(self.stmt, index))
@@ -241,20 +307,30 @@ public class Database:Hashable{
                     return Int(sqlite3_column_int64(self.stmt, index))
                 }
             }
+            /// 列数据
+            /// - Returns: 数据
             public func value()->T where T == Data{
                 let len = sqlite3_column_bytes(self.stmt, index)
                 guard let byte = sqlite3_column_blob(self.stmt, index) else { return Data() }
                 return Data(bytes: byte, count: Int(len))
             }
+            /// 列所属表名
+            /// - Returns: 表名
             public var tableName:String{
                 String(cString: sqlite3_column_table_name(self.stmt, index))
             }
+            /// 列所属数据库名
+            /// - Returns: 数据库名
             public var databaseName:String{
                 String(cString: sqlite3_column_database_name(self.stmt, index))
             }
+            /// 获取列名
+            /// - Returns: 列名
             public var name:String{
                 String(cString: sqlite3_column_name(self.stmt, index))
             }
+            
+            /// 列名
             public var type:DBDataType{
                 let a = sqlite3_column_type(self.stmt, index)
                 switch a {
@@ -274,6 +350,7 @@ public class Database:Hashable{
     }
 }
 
+/// 表信息
 public struct TableInfo{
     public let cid:Int
     public let name:String
@@ -282,6 +359,7 @@ public struct TableInfo{
     public let dlft_value:String
     public let pk:Int
 }
+
 public struct DataMasterInfo{
     public let type:String
     public let name:String
@@ -290,6 +368,7 @@ public struct DataMasterInfo{
     public let sql:String
 }
 
+/// 外键信息
 public struct TableForeignKeyInfo{
     public let id:Int
     public let seq:Int
@@ -302,6 +381,8 @@ public struct TableForeignKeyInfo{
 }
 
 extension Database{
+    /// 执行SQL
+    /// - Parameter sql: sql code
     public func exec(sql:String) throws {
         #if DEBUG
         print("SQL:"+sql)
@@ -327,30 +408,38 @@ extension Database{
             throw NSError(domain: String(data: data, encoding: .utf8) ?? "unknow error", code: 0, userInfo: ["sql":sql])
         }
     }
+    /// 执行SQL 返回结果集合
+    /// - Parameter sql: SQL
+    /// - Returns: 结果集合
     public func query(sql:String) throws ->ResultSet{
         #if DEBUG
         print("SQL:"+sql)
         #endif
         var stmt:OpaquePointer?
-        let nextSql:UnsafeMutablePointer<UnsafePointer<CChar>?> = .allocate(capacity: 1)
-        let rc = sqlite3_prepare(self.sqlite, sql, Int32(sql.utf8.count), &stmt, nextSql)
+        let rc = sqlite3_prepare(self.sqlite, sql, Int32(sql.utf8.count), &stmt, nil)
         if rc != SQLITE_OK{
             throw NSError(domain:  Database.errormsg(pointer: self.sqlite!), code: Int(rc), userInfo: ["sql":sql])
         }
         guard let s = stmt else { throw NSError(domain: Database.errormsg(pointer: self.sqlite!), code: 0, userInfo: ["sql":sql])}
-        if let cs = nextSql.pointee{
-            let nsql = String(cString: cs)
-            nextSql.deallocate()
-            return ResultSet(stmt: s, db: self,nextSql: nsql)
-        }else{
-            return ResultSet(stmt: s, db: self)
-        }
+        return ResultSet(stmt: s, db: self)
     }
-
+    
+    /// 添加表列
+    /// - Parameters:
+    ///   - name: 表名
+    ///   - columeName: 列名
+    ///   - typedef: 数据定义
+    ///   - notnull: 是否为空
+    ///   - defaultValue: 默认值
     public func addColumn(name:String,columeName:String, typedef:String ,notnull:Bool = false ,defaultValue:String = "") throws {
         let sql = "ALTER TABLE \(name) ADD COLUMN \(columeName) \(typedef) \(notnull ? "default `\(defaultValue)`" : "" )"
         try self.exec(sql: sql)
     }
+    /// 检查点
+    /// - Parameters:
+    ///   - type: 检查点类型
+    ///   - log: 检查点出发点
+    ///   - total: 检查点上线
     public func checkpoint(type:WalMode,log:Int32,total:Int32) throws {
         let l = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
         let t = UnsafeMutablePointer<Int32>.allocate(capacity: 1)
@@ -363,18 +452,26 @@ extension Database{
             throw NSError(domain: Self.errormsg(pointer: self.sqlite), code: Int(code), userInfo: nil)
         }
     }
+    /// 事务回滚
     public func rollback() throws{
         try self.exec(sql: "ROLLBACK;")
     }
+    /// 事务提交
     public func commit() throws {
         try self.exec(sql: "COMMIT;")
     }
+    /// 开始事务
     public func begin() throws {
         try self.exec(sql: "BEGIN;")
     }
+    /// 获取错误信息
+    /// - Parameter pointer: SQLite Pointer
+    /// - Returns: 错误文本
     public static func errormsg(pointer:OpaquePointer?)->String{
         String(cString: sqlite3_errmsg(pointer))
     }
+    /// 自动检查点
+    /// - Parameter frame: 页数
     public func autoCheckpoint(frame:Int32 = 100) throws{
         let code = sqlite3_wal_autocheckpoint(self.sqlite, frame)
         if SQLITE_OK != code{
@@ -406,12 +503,17 @@ extension Database{
             print("-------------")
         }, Unmanaged.passUnretained(self).toOpaque())
     }
+    /// 同步模式
+    /// - Parameter mode: 模式
     public func synchronous(mode:synchronousMode) throws {
         try self.exec(sql: "PRAGMA synchronous = " + mode.rawValue)
     }
+    /// 设置数据库版本
+    /// - Parameter version: 版本
     public func setVersion(version:Int) throws {
         try self.exec(sql: "PRAGMA user_version=\(version)")
     }
+    /// 数据库版本
     public var version:Int{
         do{
             let rs = try self.query(sql: "PRAGMA user_version")
@@ -425,6 +527,11 @@ extension Database{
             return 0
         }
     }
+    /// 查询表信息
+    /// - Parameters:
+    ///   - type: 表类型  table view
+    ///   - name: 名称
+    /// - Returns: 表信息
     public func dataMaster(type:String,name:String? = nil) throws->[DataMasterInfo]{
         let re = try self.query(sql: "select * from sqlite_master where type=? \(name != nil ? "and name=?" : "") ")
         re.bind(index: 1).bind(value: type)
@@ -443,6 +550,9 @@ extension Database{
         re.close()
         return array
     }
+    /// 表结构
+    /// - Parameter name: 表名
+    /// - Returns: 表结构
     public func tableInfo(name:String) throws ->[String:TableInfo]{
         let r = try self.query(sql: "PRAGMA table_info(\(name))")
         var map:[String:TableInfo] = [:]
@@ -460,27 +570,54 @@ extension Database{
         r.close()
         return map
     }
+    /// 修改表名
+    /// - Parameters:
+    ///   - name: 旧表名
+    ///   - newName: 新表名
     public func alterTableName(name:String,newName:String) throws {
         let sql = "ALTER TABLE \(name) RENAME TO \(newName)"
         try self.exec(sql: sql)
     }
+    /// 删除表
+    /// - Parameter name: 表名
     public func drop(name:String) throws{
         try exec(sql: "drop table if exists \(name)")
     }
+    /// 重命名列名
+    /// - Parameters:
+    ///   - name: 表名
+    ///   - columeName: 旧列
+    ///   - newName: 新列
     public func renameColumn(name:String,columeName:String,newName:String) throws {
         try self.exec(sql: "ALTER TABLE \(name) RENAME COLUMN \(columeName) TO \(newName)")
     }
+    /// 表是否存在
+    /// - Parameter name: 表名
+    /// - Returns: 结果
     public func tableExists(name:String) throws ->Bool{
        return  try self.dataMaster(type: "table", name: name).count > 0
     }
+    /// 表复制
+    /// - Parameters:
+    ///   - to: 旧表
+    ///   - from: 新表
+    ///   - keyMaps: 列名映射
     public func copyTable(to:String,from:String,keyMaps:[String:String]) throws{
         let copySql = "INSERT INTO `\(to)`(\(keyMaps.values.joined(separator: ","))) SELECT \(keyMaps.keys.joined(separator: ",")) FROM `\(from)`"
         try self.exec(sql: copySql)
     }
+    /// 表复制
+    /// - Parameters:
+    ///   - to: 旧表
+    ///   - from: 新表
+    ///   - keys: 列名
     public func copyTable(to:String,from:String,keys:[String]) throws{
         let copySql = "INSERT INTO `\(to)` SELECT \(keys.joined(separator: ",")) FROM `\(from)`"
         try self.exec(sql: copySql)
     }
+    /// 外键信息
+    /// - Parameter name: 表名
+    /// - Returns: 外键信息
     public func tableForeignKeyInfo(name:String) throws ->[String:TableForeignKeyInfo]{
         let r = try self.query(sql: "PRAGMA foreign_key_list(\(name));")
         var map:[String:TableForeignKeyInfo] = [:]
@@ -499,6 +636,9 @@ extension Database{
         r.close()
         return map
     }
+    /// 校验完整性
+    /// - Parameter table: 表名
+    /// - Returns: 是否完整性
     public func integrityCheck(table:String = "") throws ->Bool{
         let r = try self.query(sql: table.count > 0 ? "PRAGMA INTEGRITY_CHECK(\(table))" : "PRAGMA INTEGRITY_CHECK")
         if try r.step(){
@@ -509,6 +649,7 @@ extension Database{
         r.close()
         return false
     }
+    /// 开启外键
     public var foreignKey:Bool{
         get{
             do{
@@ -527,6 +668,7 @@ extension Database{
         }
     }
     
+    /// 数据库关闭
     public func close(){
         sqlite3_close(self.sqlite)
     }
