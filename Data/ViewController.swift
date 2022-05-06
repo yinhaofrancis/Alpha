4d87c7760c17fe0af90b26804051d55a676ca53f
yinhao <yinhao@5eplay.com>
2022-05-06 17:47:53 +0800

    text detect

--- Data/ViewController.swift
+++ Data/ViewController.swift
@@ -7,30 +7,48 @@
 
 import UIKit
 import Ammo
-class ViewController: UIViewController {
+import TextDetect
+class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
+    @IBAction func detect(_ sender: Any) {
+        let c = UIImagePickerController()
+        c.delegate = self
+        c.sourceType = .camera
+        self.showDetailViewController(c, sender: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        for _ in 0 ..< (arc4random() % 100){
-            DispatchQueue.global().async {
-                self.db.workflow { db in
-                    let json = JSONObject(json: ["ddd":"s"])
-                    try json.declare.create(db: db)
-                    try json.save(db: db)
-                }
-                try! self.db.query { db in
-                    let json:[JSONObject] = try JSONObject.select(db: db)
-                    print(json)
-                }
-            }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        super.touchesBegan(touches, with: event)
+//        for _ in 0 ..< (arc4random() % 100){
+//            DispatchQueue.global().async {
+//                self.db.workflow { db in
+//                    let json = JSONObject(json: ["ddd":"s"])
+//                    try json.declare.create(db: db)
+//                    try json.save(db: db)
+//                }
+//                try! self.db.query { db in
+//                    let json:[JSONObject] = try JSONObject.select(db: db)
+//                    print(json)
+//                }
+//            }
+//        }
+//    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let img:UIImage = info[.originalImage] as? UIImage else { return }
+        print(img)
+        
+        try! self.dt.detect(image: img.cgImage!) { strs in
+            print(strs)
         }
+        picker.dismiss(animated: true)
     }
 
     @DBWorkFlow(name:"mark")
     public var db:DataBaseWorkFlow
+    
+    var dt:TextDetect = TextDetect()
 
 }
 
---
+++ TextDetect/TextDetect.swift
@@ -0,0 +1,35 @@
+//
+//  TextDetect.swift
+//  TextDetect
+//
+//  Created by hao yin on 2022/5/6.
+//
+
+import Foundation
+import Vision
+
+
+public class TextDetect{
+    public init() {}
+    public func detect(image:CGImage,callback:@escaping ([String])->Void) throws{
+        var texts:[String] = []
+        let textRecognitionRequest = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else {
+                print("The observations are of an unexpected type.")
+                return
+            }
+            // 把识别的文字全部连成一个string
+            let maximumCandidates = 1
+            for observation in observations {
+                guard let candidate = observation.topCandidates(maximumCandidates).first else { continue }
+                texts.append(candidate.string)
+            }
+            callback(texts)
+        }
+        textRecognitionRequest.recognitionLevel = .accurate
+        textRecognitionRequest.recognitionLanguages = ["zh-cn","en-US"]
+        textRecognitionRequest.revision = VNRecognizeTextRequestRevision2
+        textRecognitionRequest.usesLanguageCorrection = true
+        try VNImageRequestHandler(cgImage: image).perform([textRecognitionRequest])
+    }
+}
