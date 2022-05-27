9936b3ae1131ca4d3f6858189b3de80cc9cbf696
yinhao <yinhao@5eplay.com>
2022-05-27 18:13:25 +0800

    dsdsd

--- AmmoTests/AmmoTests.swift
+++ AmmoTests/AmmoTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import Data
 class AmmoTests: XCTestCase {
 
+    @DBWorkFlow(name: "data")
+    var queue:DataBaseWorkFlow
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -20,27 +22,30 @@ class AmmoTests: XCTestCase {
 
 
     
-    func testExample() throws {
-        let ctx = Context(size: CGSize(width: 100, height: 100), scale: 3)
-        
-        ctx.begin { ctx in
-            ctx.context.setFillColor(UIColor.red.cgColor)
-            ctx.context.fill(CGRect(x: 10, y:10, width: 80, height: 10))
-            let ii = UIImage(named: "i")!.cgImage
-//            ctx.drawImage(image: ii!, rect: CGRect(x: 50, y: 50, width: 50, height: 50))
-            ctx.drawScaleImage(image: ii!, rect: CGRect(x: 30, y: 30, width: 50, height: 50),mode: .fillScale)
-            
-            let a = NSAttributedString(string: "dsds|dsds", attributes: [
-                .font:UIFont.systemFont(ofSize: 20),
-                .foregroundColor:UIColor.red
-            ])
-            let pathframe = CGRect(x: 30, y: 30, width: 30, height: 30)
-            ctx.drawString(string: a as CFAttributedString, constaint: pathframe)
+    func testExample() async throws {
+//        let ctx = Context(size: CGSize(width: 100, height: 100), scale: 3)
+//
+//        ctx.begin { ctx in
+//            ctx.context.setFillColor(UIColor.red.cgColor)
+//            ctx.context.fill(CGRect(x: 10, y:10, width: 80, height: 10))
+//            let ii = UIImage(named: "i")!.cgImage
+////            ctx.drawImage(image: ii!, rect: CGRect(x: 50, y: 50, width: 50, height: 50))
+//            ctx.drawScaleImage(image: ii!, rect: CGRect(x: 30, y: 30, width: 50, height: 50),mode: .fillScale)
+//
+//            let a = NSAttributedString(string: "dsds|dsds", attributes: [
+//                .font:UIFont.systemFont(ofSize: 20),
+//                .foregroundColor:UIColor.red
+//            ])
+//            let pathframe = CGRect(x: 30, y: 30, width: 30, height: 30)
+//            ctx.drawString(string: a as CFAttributedString, constaint: pathframe)
+//
+//        }
+//        let img = ctx.image!
+//
+//        print(img)
+        try await self.queue.asyncWorkflow { db in
             
         }
-        let img = ctx.image!
-        
-        print(img)
     }
 
     func testPerformanceExample() throws {
@@ -51,4 +56,3 @@ class AmmoTests: XCTestCase {
     }
 
 }
-
