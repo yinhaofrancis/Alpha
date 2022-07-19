9ff9a2c0df6cabc4fefc251cfd73beabf37c7993
yinhao <yinhao@5eplay.com>
2022-07-19 21:39:51 +0800

    Update Context.swift

--- Ammo/View/Context.swift
+++ Ammo/View/Context.swift
@@ -17,7 +17,7 @@ public enum DrawImageMode{
     case scale
 }
 
-public struct Context{
+public struct RenderContext{
     public private(set) var context:CGContext
     public init(size:CGSize,scale:CGFloat){
         let w = Int(size.width * scale)
@@ -26,18 +26,18 @@ public struct Context{
         self.context.translateBy(x: 0, y: -size.height)
     }
     
-    public func begin(call:(Context)->Void){
+    public func begin(call:(RenderContext)->Void){
         self.context.saveGState()
         call(self)
         self.context.restoreGState()
     }
     @available(iOS 13.0, *)
-    public func render(_ call:@escaping (Context)->Void) async ->CGImage?{
+    public func render(_ call:@escaping (RenderContext)->Void) async ->CGImage?{
         return await Task {
             self.syncRender(call)
         }.value
     }
-    public func syncRender(_ call:(Context)->Void) ->CGImage?{
+    public func syncRender(_ call:(RenderContext)->Void) ->CGImage?{
         self.begin(call:call)
         return self.image
     }
@@ -109,50 +109,16 @@ public struct Context{
         let drawFrame =  CGRect(x: constaint.minX, y: constaint.minY, width: display.width, height: display.height)
         self.drawFrame(frame: frame, rect:drawFrame)
     }
+    public func drawString(string:CFAttributedString,point:CGPoint){
+        self.drawString(string: string, constaint: CGRect(origin: point, size: CGSize(width: CGFloat.infinity, height: .infinity)))
+    }
+    public static func size(string:CFAttributedString,transform:CGAffineTransform)->CGSize{
+        let frameset = CTFramesetterCreateWithAttributedString(string)
+        let range = CFRange(location: 0, length: CFAttributedStringGetLength(string))
+        let display = CTFramesetterSuggestFrameSizeWithConstraints(frameset,range , nil, CGSize(width: CGFloat.infinity, height: .infinity), nil);
+        return display.applying(transform);
+    }
     public var image:CGImage?{
         return self.context.makeImage()
-    }
-}
-
-public var nodeKey:String = "nodeKey"
-
-extension UIView:LayoutContent{
-    public func layout(){
-        self.loadNode()
-        self.node.width = Value(constant: self.frame.width, mode: .absoluteValue)
-        self.node.height = Value(constant: self.frame.height, mode: .absoluteValue)
-        self.node.layout()
-        self.active()
-    }
-    
-    private func active(){
-        self.frame = self.node.resultFrame
-        for i in self.subviews{
-            i.active()
-        }
-    }
-    private func loadNode(){
-        self.node.children = self.subviews.map({$0.node})
-        for i in self.subviews{
-            i.loadNode()
-        }
-    }
-    public var node:Node{
-        var current = objc_getAssociatedObject(self, &nodeKey)
-        if current == nil{
-            let node = Node()
-            current = node
-            node.grows = 1
-            node.shrink = 1
-            node.align = .fill
-            node.justify = .start
-            node.direction = .vertical
-            node.layoutContent = self
-        }
-        objc_setAssociatedObject(self, &nodeKey, current, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-        return current as! Node
-    }
-    public func fitSize(constaint: CGSize) -> CGSize {
-        self.systemLayoutSizeFitting(constaint)
     }
 }
